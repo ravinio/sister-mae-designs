b8fdcb4829570e949cfb899f5813615533b85a69
rio <rio@onestaffmedical.com>
2024-06-14 11:20:41 -0500

    minor styling setups

--- src/theme.js
+++ src/theme.js
@@ -8,6 +8,14 @@ const theme = createTheme({
     secondary: {
       main: '#ff23ed',
     },
+    text: {
+      primary: '#40151B',
+      secondary: '#9F613E',
+      disabled: 'rgba(255,255,255,.5)'
+    },
+    background: {
+      // default: 'radial-gradient(97.24% 68.2% at 63.23% 57.64%, rgba(216, 191, 197, 0.35) 6.45%, rgba(181, 134, 146, 0.35) 40.11%, rgba(179, 121, 109, 0.35) 67.69%, rgba(175, 115, 104, 0.35) 78.08%, rgba(132, 65, 55, 0.35) 100%), #FFF'
+    },
   },
 });
 
