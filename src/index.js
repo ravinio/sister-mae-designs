d10b9260dff75945be32a37d13efea1d1c5dcaee
Ravin Io <rio@onestaffmedical.com>
2024-06-14 11:31:14 -0500

    add globalstyles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import theme from './theme';
+
+const globalStyles = (
+  <GlobalStyles
+    styles={{
+      body: {
+        background: `${theme.palette.background.default} !important`,
+        margin: 0,
+        padding: 0,
+        boxSizing: 'border-box',
+        minHeight: '100vh',
+      },
+    }}
+  />
+);
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
@@ -11,6 +26,7 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
+    {globalStyles}
     <App />
   </ThemeProvider>,
 );
